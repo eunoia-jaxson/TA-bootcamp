41f7bcde4031ba37371e17eefaa5a0d8701f3de0
eunoia-jaxson <eunoia@handong.ac.kr>
2024-08-19 13:16:21 +0900

    사용자 입력 및 스트라이크 볼 판별 로직구현
    사용자가 입력한 숫자를 배열에 저장하고 computer 배열과 user배열을 이중 for문으로 비교하여 스트라이크, 볼 판별하는 로직 구현.

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
     System.out.println("숫자 야구 게임을 시작합니다.");
     
     while (start == "Y") {
+
       // 서로 다른 세 자리 숫자 생성
       int[] computer = new int[3];
       for (int i = 0; i < computer.length; i++) {
@@ -21,8 +22,19 @@ public class Main {
         }
       }
 
+
+      // 사용자로부터 세 자리 숫자 입력받기
       System.out.println("숫자를 입력해주세요: ");
       int number = scanner.nextInt();
+      int[] user = new int[3];
+
+      // 사용자가 입력한 숫자를 배열에 저장
+      for (int i = user.length - 1; i >= 0; i--) {
+        user[i] = number % 10;
+        number /= 10;
+      }
+
+      System.out.println("사용자가 입력한 숫자: " + user[0] + user[1] + user[2]);
       
       System.out.println("게임을 새로 시작하려면 Y, 종료하려면 N을 입력하세요.");
       start = scanner.next();
