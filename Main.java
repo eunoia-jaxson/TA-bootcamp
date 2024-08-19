cfef465bdd42d18066e69d2b3aa2df5d89faf18d
eunoia-jaxson <eunoia@handong.ac.kr>
2024-08-19 13:03:47 +0900

    숫자 야구 게임 시작 - Main 생성, 세팅
    메인 클래스에서 숫자야구 게임을 시작하는 인터페이스와
    정답 숫자 랜덤 생성

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// 숫자 야구 게임
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String start = "Y";
+
+    System.out.println("숫자 야구 게임을 시작합니다.");
+    
+    while (start == "Y") {
+      // 서로 다른 세 자리 숫자 생성
+      int[] computer = new int[3];
+      for (int i = 0; i < computer.length; i++) {
+        computer[i] = (int)(Math.random() * 9) + 1;
+        for (int j = 0; j < i; j++) {
+          if (computer[i] == computer[j]) {
+            i--;
+            break;
+          }
+        }
+      }
+
+      System.out.println("숫자를 입력해주세요: ");
+      int number = scanner.nextInt();
+      
+      System.out.println("게임을 새로 시작하려면 Y, 종료하려면 N을 입력하세요.");
+      start = scanner.next();
+    }
+  }
+}
