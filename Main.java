232b5699847884c97840e3f5aee62103fa5cb35d
eunoia-jaxson <eunoia@handong.ac.kr>
2024-08-19 13:25:57 +0900

    결과 출력 후, 재시작 기능
    strike, ball 변수의 개수 체크 후 출력.
    다 맞췄을 시 종료 후, 재시작 여부를 받음.
    
    Y 입력 시, 재시작.
    N 입력 시, 그대로 종료.

--- Main.java
+++ Main.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    String start = "Y";
 
     System.out.println("숫자 야구 게임을 시작합니다.");
     
-    while (start == "Y") {
-
+    while (true) {
       // 서로 다른 세 자리 숫자 생성
       int[] computer = new int[3];
       for (int i = 0; i < computer.length; i++) {
@@ -21,23 +19,55 @@ public class Main {
           }
         }
       }
+      System.out.println("컴퓨터가 숫자를 생성했습니다: " + computer[0] + computer[1] + computer[2]);
 
+      while (true) {
+        // 사용자로부터 세 자리 숫자 입력받기
+        System.out.print("숫자를 입력해주세요: ");
+        int number = scanner.nextInt();
+        int[] user = new int[3];
 
-      // 사용자로부터 세 자리 숫자 입력받기
-      System.out.println("숫자를 입력해주세요: ");
-      int number = scanner.nextInt();
-      int[] user = new int[3];
+        // 사용자가 입력한 숫자를 배열에 저장
+        for (int i = user.length - 1; i >= 0; i--) {
+          user[i] = number % 10;
+          number /= 10;
+        }
 
-      // 사용자가 입력한 숫자를 배열에 저장
-      for (int i = user.length - 1; i >= 0; i--) {
-        user[i] = number % 10;
-        number /= 10;
+        // 스트라이크, 볼 판별
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < computer.length; i++) {
+          for (int j = 0; j < user.length; j++) {
+            if (computer[i] == user[j]) {
+              if (i == j) {
+                strike++;
+              } else {
+                ball++;
+              }
+            }
+          }
+        }
+
+        // 결과 출력
+        if (strike == 3) {
+          System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+          break;
+        } else if (strike == 0 && ball == 0) {
+          System.out.println("OUT");
+        } else if (strike == 0) {
+          System.out.println(ball + "B");
+        } else if (ball == 0) {
+          System.out.println(strike + "S");
+        } else {
+          System.out.println(ball + "B " + strike + "S");
+        }
       }
-
-      System.out.println("사용자가 입력한 숫자: " + user[0] + user[1] + user[2]);
       
       System.out.println("게임을 새로 시작하려면 Y, 종료하려면 N을 입력하세요.");
-      start = scanner.next();
+      String start = scanner.next();
+      if (start.equals("N")) {
+        break;
+      }
     }
   }
 }
